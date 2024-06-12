14161ad6795c71ccdb1e8bbed97d129e04571370
vshas27 <vsahas27@icloud.com>
2024-06-12 19:16:51 +0530

    how to use operators on js

--- Basics/operators.js
+++ Basics/operators.js
@@ -3,3 +3,22 @@
 let x = 7,z = 7;
 let sum = x + z;
 console.log(sum);
+
+//subraction -
+
+let s = 8,g = 88;
+let subract = g - s;
+console.log(subract);
+
+//multiplication *
+
+let b = 6,p = 12;
+let multiply = b * g;
+console.log(multiply);
+
+//division /
+
+let v = 42,c =7;
+let divide = v / c;
+console.log(divide);
+
